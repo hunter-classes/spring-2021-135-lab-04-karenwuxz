36bd7f44adc66eb736fdd3b289e7203f353974b8
Karen Wu <karenwu@Karens-MacBook-Pro.local>
2021-02-23 17:33:18 -0500

    Main file

---
+++ main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include "box.h"
+#include "checkerBoard.h"
+
+int main(){
+    // std::cout << "box(4,4):\n" << box(4,4) << "\n------------------\n";
+    // std::cout << "box(5,2):\n" << box(5,2) << "\n------------------\n";
+    // std::cout << "box(10,3):\n" << box(10,3) << "\n------------------\n";
+
+    std::cout << "checkers(11,6):\n" << checkers(11,6) << "\n------------------\n";
+    std::cout << "checkers(3,4):\n" << checkers(3,4) << "\n------------------\n";
+    std::cout << "checkers(10,3):\n" << checkers(10,3) << "\n------------------\n";
+
+    return 0;
+}
