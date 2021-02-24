3a40540205e2cd4b9034f9935460a6fa20dad54f
Karen Wu <karenwu@Karens-MacBook-Pro.local>
2021-02-24 18:54:27 -0500

    Updated

--- main.cpp
+++ main.cpp
@@ -2,14 +2,32 @@
 #include "box.h"
 #include "checkerBoard.h"
 
+#include "lower.h"
+#include "upper.h"
+#include "trapezoid.h"
 int main(){
     // std::cout << "box(4,4):\n" << box(4,4) << "\n------------------\n";
     // std::cout << "box(5,2):\n" << box(5,2) << "\n------------------\n";
     // std::cout << "box(10,3):\n" << box(10,3) << "\n------------------\n";
 
-    std::cout << "checkers(11,6):\n" << checkers(11,6) << "\n------------------\n";
-    std::cout << "checkers(3,4):\n" << checkers(3,4) << "\n------------------\n";
-    std::cout << "checkers(10,3):\n" << checkers(10,3) << "\n------------------\n";
+    // std::cout << "checkers(11,6):\n" << checkers(11,6) << "\n------------------\n";
+    // std::cout << "checkers(3,4):\n" << checkers(3,4) << "\n------------------\n";
+    // std::cout << "checkers(10,3):\n" << checkers(10,3) << "\n------------------\n";
 
+
+    // std::cout<< "lower(6):\n" << lower(6) << "\n------------------\n";
+    // std::cout<< "lower(3):\n" << lower(3) << "\n------------------\n";
+    // std::cout<< "lower(10):\n" << lower(10) << "\n------------------\n";
+
+    // std::cout<< "upper(5):\n" << upper(5) << "\n------------------\n";
+    // std::cout<< "upper(3):\n" << upper(3) << "\n------------------\n";
+    // std::cout<< "upper(10):\n" << upper(10) << "\n------------------\n";
+
+    std::cout << "trap(5,3):\n" << trap(5,3) << "\n------------------\n";
+    std::cout << "trap(12,5):\n" << trap(12,5) << "\n------------------\n";
+    std::cout << "trap(12,7):\n" << trap(12,7) << "\n------------------\n";
+
+
+    
     return 0;
 }
